98318b4081ca58266f6a447a396f786907dfc3aa
Student <student@post.bgu.ac.il>
2026-01-11 16:04:19 +0000

    2

--- server/src/main/java/bgu/spl/net/api/StompMessagingProtocolImpl.java
+++ server/src/main/java/bgu/spl/net/api/StompMessagingProtocolImpl.java
@@ -3,11 +3,14 @@ package bgu.spl.net.api;
 import javax.print.DocFlavor.STRING;
 
 import bgu.spl.net.impl.data.Database;
+import bgu.spl.net.impl.data.LoginStatus;
 import bgu.spl.net.srv.Connections;
 
 public class StompMessagingProtocolImpl implements StompMessagingProtocol<String>{
     private int connectionId;
     private Connections<String> connections;
+    String currentUser;
+    boolean shouldTerminate = false;
 
     public void start(int connectionId, Connections<String> connections){
         this.connectionId = connectionId;
@@ -62,7 +65,29 @@ public class StompMessagingProtocolImpl implements StompMessagingProtocol<String
             return;
         }
         Database db = Database.getInstance();
-        db.login(connectionId, login, passcode);
+        LoginStatus status = db.login(connectionId, login, passcode);
+        if (status == LoginStatus.LOGGED_IN_SUCCESSFULLY || status == LoginStatus.ADDED_NEW_USER) {
+            this.currentUser = login;
+            connections.send(connectionId, "CONNECTED\nversion:1.2\n\n");
+        } else {
+            String errorHeader = "";
+            String errorBody = "";
+            if (status == LoginStatus.WRONG_PASSWORD) {
+                errorHeader = "Wrong password";
+                errorBody = "Password does not match the username";
+            } else if (status == LoginStatus.ALREADY_LOGGED_IN) {
+                errorHeader = "User already logged in";
+                errorBody = "User is already logged in active session";
+            } else if (status == LoginStatus.CLIENT_ALREADY_CONNECTED) {
+                errorHeader = "Client already connected";
+                errorBody = "This connection is already associated with a user";
+            }
+
+        
+            connections.send(connectionId, "ERROR\nmessage:" + errorHeader + "\n\n" + errorBody);
+            shouldTerminate = true;
+            connections.disconnect(connectionId);
+    }
     }
 
     public void processSend(String[] lines) {
